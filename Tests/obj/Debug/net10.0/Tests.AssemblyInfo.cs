84d39f85202aa749b06a345510cc36c39c55b8ba
Student <foobar@sr.htlweiz.at>
2026-04-20 11:45:29 +0200

    in_work

--- Tests/obj/Debug/net10.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net10.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb1a3d80c59a5345ddad8169e0ddd5966508b5f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717518a3573d58adb6d8d520c13d5b027fb67a9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
